45dd965c3810ca23700e5b1d5d4b7dfd10c68d18
José Juan Ojeda Granados <jotafirebase@gmail.com>
2023-02-23 13:55:56 +0000

    23-02

--- src/ejer_rust_en_sp/ejer_basicos.rs
+++ src/ejer_rust_en_sp/ejer_basicos.rs
@@ -22,12 +22,6 @@
                 https://doc.rust-lang.org/std/index.html
     Crate gtk   https://gtk-rs.org/gtk3-rs/git/docs/gtk/index.html
 
-    Dependencias:
-
-    Compilar:
-
-    Licencia:
-
 ***************************************************************************************/
 
 fn imprime_titulo(titulo: &String){
